b5c127509bbafa873196389d2c4b5d9d6568abe8
Haydar <turborg2011@gmail.com>
2023-08-25 16:31:02 +0700

    Initial Commit

---
+++ AvitoIntern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AvitoIntern
+//
+//  Created by Хайдар Даукаев on 25.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
